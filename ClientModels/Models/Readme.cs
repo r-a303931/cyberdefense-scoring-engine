a20f47c9a7c64c7e1b8930f0b6d6c335207875ee
Andrew Rioux <andrew.rioux@mdwg.cap.gov>
2021-06-14 20:41:05 -0700

    Added framework for different clients

---
+++ ClientModels/Models/Readme.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientModels
+{
+	public class Readme
+	{
+		public int ID { get; set; }
+
+		public string Text { get; set; }
+	}
+}
---
+++ Windows/WindowsClient/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace WindowsClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            IPAddress address = new IPAddress(new byte[] { 127, 0, 0, 1 });
+            IPEndPoint endPoint = new IPEndPoint(address, 54248);
+            Socket listener = new Socket(address.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            Console.WriteLine("Starting to bind...");
+
+            listener.Bind(endPoint);
+            listener.Listen(10);
+
+            Console.WriteLine("Bound port");
+
+            while (true)
+            {
+                Console.WriteLine("Accepting connection...");
+                var client = await listener.AcceptAsync();
+                Console.WriteLine("Got connection");
+
+                client.Send(Encoding.ASCII.GetBytes("Hello World!"));
+
+                client.Close();
+            }
+        }
+    }
+}
