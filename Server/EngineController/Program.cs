451b8c8998a2f944f635da70c707a53f05cb991f
Andrew Rioux <rioux.d.andrew@gmail.com>
2021-06-27 23:55:09 -0400

    Made sure database migrates appropriately

--- Server/EngineController/Program.cs
+++ Server/EngineController/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -38,6 +39,7 @@ namespace EngineController
             {
                 var context = services.GetRequiredService<Data.EngineControllerContext>();
                 context.Database.EnsureCreated();
+                context.Database.Migrate();
             }
             catch (Exception ex)
             {
